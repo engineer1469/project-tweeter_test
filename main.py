fed4b33c67e5c6b098a267df9340f48c19c3c2f6
Tudor <tudormatei010@gmail.com>
2023-11-02 17:11:06 +0100

    f

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
 import neca
 from neca.events import *
 from neca.generators import generate_data
+from neca.events import create_context, Ruleset
 from neca.events import emit
+from neca.settings import app, socket
+from flask import Flask, request, send_file
+from wordcloud import WordCloud
+
+import random
+
+filter = create_context("Filter")
+text_data = create_context("Texts")
+
+# Words to ingnore in the wordcloud
+ignore_words = [
+    "https", "http", "RT", "co", "t", "v", "and", "the", "a", "CO",
+    "with", "to", "in", "of", "by", "at", "you", "she", "he", "than", "from",
+    "then", "not", "for", "It", "it", "I", "was", "your", "are", "is", "on",
+    "the", "an", "is", "that", "on", "as", "an", "in", "or", "at", "who", "which",
+    "our", "by", "this", "these", "those", "with", "when", "where", "why", "how",
+    "can", "will", "should", "could", "would", "did", "does", "do", "am", "were",
+    "be", "has", "have", "had", "up", "down", "out", "over", "under", "off", "on",
+    "above", "below", "between", "among", "beside", "before", "after", "during",
+    "while", "since", "against", "through", "into", "onto", "upon", "about", "towards"
+]
+
+# Color for the word cloud
+colors = [
+    "#FF6384",
+    "#36A2EB",
+    "#4BC0C0",
+    "#BC3A4E",
+    "#701FBF",
+    "#E4E533",
+    "#ECAF14",
+    "#3B344E" 
+]
+
+def specific_colors(word, font_size, position, orientation, random_state=None, **kwargs):
+    return random.choice(colors)
+
+@app.route('/generate_wordcloud')
+def generate_wordcloud():
+    wordcloud = WordCloud(width=300, height=200, background_color='white', collocations=False,
+        color_func=specific_colors, max_words=1000, stopwords=ignore_words).generate(text_data['texts'])
+
+    wordcloud.to_file("wordcloud.png")
+
+    return send_file("wordcloud.png", mimetype='image/png')
 
 @event("init")
 def init(context, data):
-    # Calls events based on parameters
     generate_data(
         data_file = "sports-20191117.txt",
-        time_scale = 1,
+        time_scale = .5,
         event_name = 'tweet',
         limit = 100000
     )
@@ -33,21 +78,32 @@ def identify_sport(text):
     
     return 'none'
 
+@app.route('/buttons', methods=['POST'])
+def route():
+    data = request.json 
+    filter['filter'] = data['id']
+    return 'caught response', 200
 
 @event("tweet")
 def my_event_handler(context, data):
-    emit("tweet_stream", data)
+    if 'tennis' in data['text'].lower():
+        emit("tennisTweetStream", data)
+
+    current_filter = filter['filter']
+    if current_filter == 'none':
+        emit("tweet_stream", data)
+    elif current_filter in data['text'].lower():
+        emit("tweet_stream", data)
+    else:
+        return None
 
     text_to_search = data['text'].lower()
     sport = identify_sport(text_to_search)
 
     if sport != 'none':
         emit("piechart", {"action": "add", "value": [sport, 1]})
+        text_data['texts'] += data['text']
 
-
-@event("search")
-def searh(keyword):
-    print(keyword)
-    return "testmessage"
-
+filter['filter'] = 'none'
+text_data['texts'] = 'sport'
 neca.start()
--- static/lib/buttons.js
+++ static/lib/buttons.js
@@ -1,6 +1,44 @@
 function handleClick(id) {
-    
-
-    alert("The sport clicked from external script!");
-    alert(id);
+  console.log("The button clicked was" + id);
+  fetch("/buttons", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id }),
+  });
 }
+
+function getWordCloud() {
+  console.log("Getting current word cloud...");
+  fetch("/generate_wordcloud", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return response.blob();
+      } else {
+        console.error("Failed to fetch word cloud.");
+      }
+    })
+    .then((blob) => {
+      const url = URL.createObjectURL(blob);
+      const img = new Image();
+      img.src = url;
+
+      const wordcloudDiv = document.getElementById("wordcloud");
+      wordcloudDiv.innerHTML = "";
+      wordcloudDiv.appendChild(img);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+getWordCloud(); 
+
+setInterval(getWordCloud, 5000); // Getting wordcloud every 5 seconds
